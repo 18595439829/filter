d52e5fda6b70b6dfc8d4270a54aaea253d79dc6b
kaiyuanweb <webkaiyuan@126.com>
2018-08-21 18:38:50 +0800

    过滤器

---
+++ filter/src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>This is an about page {{RMB | capitalize}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+      RMB: 1
+    }
+  },
+  filters: {
+    capitalize: function(value) {
+      if (!value) return ''
+      value = value.toString()
+      if (value == '1') {
+        return '男'
+      } else {
+        return '女'
+      }
+    }
+  }
+}
+</script>
